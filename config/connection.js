23a4773e953b2d90899b1ae63c1a8ce0b6109611
JPlukarski <jwplukarski@gmail.com>
2018-09-17 20:36:09 -0500

    removed jaws_db

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,16 @@
 var mysql = require("mysql");
-var connection;
 
-if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-    var connection = mysql.createConnection({
-        host: "localhost",
-        port: 3306,
-        user: "root",
-        password: "root",
-        database: "burgers_db",
-        socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock"
-    });
-}
+
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "burgers_db",
+    socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock"
+});
+
 
 
 connection.connect(function (err) {
